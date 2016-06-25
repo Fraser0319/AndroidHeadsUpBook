726dab75de75fe372cb90cb5ac1eb862004728e7
Fraser Beaton <xdb14182@uni.strath.ac.uk>
2016-06-26 00:35:36 +0100

    finished chapter 6 and added code from exercises

--- HeadsUpChapter6StarbuzzApp/app/src/main/java/com/example/fraserbeaton/headsupchapter6starbuzzapp/TopLevelActivity.java
+++ HeadsUpChapter6StarbuzzApp/app/src/main/java/com/example/fraserbeaton/headsupchapter6starbuzzapp/TopLevelActivity.java
@@ -1,7 +1,12 @@
 package com.example.fraserbeaton.headsupchapter6starbuzzapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
+
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        //create onItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    Intent intent = new Intent(TopLevelActivity.this,DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        //add the listener to the list view
+        ListView listview = (ListView) findViewById(R.id.list_options);
+        listview.setOnItemClickListener(itemClickListener);
     }
 }
